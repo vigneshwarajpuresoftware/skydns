e9f1bf611c5476a78a6afea926456082c219ede7
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-09-20 10:18:26 +0530

    Update main.go

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 	"github.com/skynetservices/skydns/server"
 
 	etcd "github.com/coreos/etcd/client"
-	etcdv3 "go.etcd.io/etcd/client/v3"
+	etcdv3 "go get go.etcd.io/etcd/clientv3@v3.3.23"
 	"github.com/coreos/etcd/pkg/transport"
 	"github.com/miekg/dns"
 )
